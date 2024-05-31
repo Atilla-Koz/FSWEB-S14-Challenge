bb9119813651ee79542547a7d577f1adad7792a6
Atilla-Koz <atillakoz@hotmail.com>
2024-05-31 13:02:42 +0300

    all class finis

--- src/main/java/org/example/DeluxBurger.java
+++ src/main/java/org/example/DeluxBurger.java
@@ -1,4 +1,45 @@
 package org.example;
 
-public class DeluxBurger {
+public class DeluxBurger extends Hamburger{
+
+    private  String chips;
+    private  String drink;
+
+    public DeluxBurger(){
+
+        super("Delux Burger","Double",19.10,"Double Sandwich");
+        this.chips = "Curvy";
+        this.drink = "Coke";
+    }
+
+    public  String getChips(){
+        return chips;
+    }
+
+    public  String getDrink(){
+        return drink;
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Deluxe Burger için yeni malzeme eklenemez.");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Deluxe Burger için yeni malzeme eklenemez.");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Deluxe Burger için yeni malzeme eklenemez.");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Deluxe Burger için yeni malzeme eklenemez.");
+    }
+
+
+
 }
--- src/main/java/org/example/HeathyBurger.java
+++ src/main/java/org/example/HeathyBurger.java
@@ -1,4 +1,40 @@
 package org.example;
 
-public class HeathyBurger {
+public class HeathyBurger extends Hamburger{
+
+    private String healtyExtra1Name;
+    private double healtyExtra1Price;
+
+    private  String healtyExtra2Name;
+    private double healtyExtra2Price;
+
+    public HealthyBurger(String name, double price, String breadRollType) {
+        super("Healthy", "tofu ", price, breadRollType);
+
+    }
+
+
+    public void addHealthyAddition1(String name, double price) {
+        this.healtyExtra1Name = name;
+        this.healtyExtra1Price = price;
+    }
+
+    public void addHealthyAddition2(String name, double price) {
+        this.healtyExtra2Name = name;
+        this.healtyExtra2Price = price;
+    }
+
+    @Override
+    public double itemizeHamburger() {
+        double hamburgerPrice = super.itemizeHamburger();
+        if (this.healtyExtra1Name != null) {
+            hamburgerPrice += this.healtyExtra1Price;
+            System.out.println("Added " + this.healtyExtra1Name + " for an extra " + this.healtyExtra1Price);
+        }
+        if (this.healtyExtra2Name != null) {
+            hamburgerPrice += this.healtyExtra2Price;
+            System.out.println("Added " + this.healtyExtra2Name + " for an extra " + this.healtyExtra2Price);
+        }
+        return hamburgerPrice;
+    }
 }
